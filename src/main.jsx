2f049aef78a90efa8ca327890fa7ca480c7ae39f
athenszz00 <athenszz00@gmail.com>
2026-09-04 16:17:09 +0700

    tambah Logo

--- src/main.jsx
+++ src/main.jsx
@@ -52,21 +52,21 @@ import medcoLogo from './assets/clients/medco.png';
 import cikarangLogo from './assets/clients/cikarang-listrindo.png';
 import tripatraLogo from './assets/clients/tripatra.png';
 import pjbLogo from './assets/clients/pjb.png';
-import binaguna from './assets/clients/binaguna.png';
-import bptangguh from './assets/clients/bptangguh.png';
-import controlsystems from './assets/clients/controlsystems.png';
-import donggisenoro from './assets/clients/donggisenoro.png';
-import endresshauser from './assets/clients/endresshauser.png';
-import gkt from './assets/clients/gkt.jpg';
-import gunaeramanufaktura from './assets/clients/gunaeramanufaktura.jpg';
-import imecointersarana from './assets/clients/imecointersarana.jpg';
-import indokomas from './assets/clients/indokomas.png';
-import ipmomipaiton from './assets/clients/ipmomipaiton.png';
-import ismaya from './assets/clients/ismaya.jpg';
-import istanakaranglaut from './assets/clients/istanakaranglaut.jpg';
-import kni from './assets/clients/kni.jpg';
-import kokusaikeiso from './assets/clients/kokusaikeiso.png';
-import kpp from './assets/clients/kpp.png';
+import binagunaLogo from './assets/clients/binaguna.png';
+import bptangguhLogo from './assets/clients/bptangguh.png';
+import controlsystemsLogo from './assets/clients/controlsystems.png';
+import donggisenoroLogo from './assets/clients/donggisenoro.png';
+import endresshauserLogo from './assets/clients/endresshauser.png';
+import gktLogo from './assets/clients/gkt.jpg';
+import gunaeramanufakturaLogo from './assets/clients/gunaeramanufaktura.jpg';
+import imecointersaranaLogo from './assets/clients/imecointersarana.jpg';
+import indokomasLogo from './assets/clients/indokomas.png';
+import ipmomipaitonLogo from './assets/clients/ipmomipaiton.png';
+import ismayaLogo from './assets/clients/ismaya.jpg';
+import istanakaranglautLogo from './assets/clients/istanakaranglaut.jpg';
+import kniLogo from './assets/clients/kni.jpg';
+import kokusaikeisoLogo from './assets/clients/kokusaikeiso.png';
+import kppLogo from './assets/clients/kpp.png';
 
 /* =========================================================
    SERVICES DATA
